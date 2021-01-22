80f45db733633a9d0eaf51b51ad3d3a02cd27038
ilromali <iliashenkoroman@gmail.com>
2021-01-22 11:58:19 -0800

    Add full CRUD fxnlty for parks & locations

---
+++ ParkAPI/Controllers/LocationsController.cs
@@ -0,0 +1,74 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ParkFinder.Models;
+
+namespace ParkFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private ParkFinderContext _db;
+        public LocationsController(ParkFinderContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/locations
+        [HttpGet]
+        public ActionResult<IEnumerable<Location>> Get(string city, string state, string country, string region)
+        {
+            var query = _db.Locations.AsQueryable();
+
+            if (city != null)
+            {
+                query = query.Where(entry => entry.City == city);
+            }
+
+            if (state != null)
+            {
+                query = query.Where(entry => entry.State == state);
+            }
+
+            if (country != null)
+            {
+                query = query.Where(entry => entry.Country == country);
+            }
+
+            if (region != null)
+            {
+                query = query.Where(entry => entry.Region == region);
+            }
+
+            return query.ToList();
+        }
+
+        //POST api/locations
+        [HttpPost]
+        public void Post([FromBody] Location location)
+        {
+            _db.Locations.Add(location);
+            _db.SaveChanges();
+        }
+
+        //GET api/locations/5
+        [HttpGet("{id}")]
+        public void Put(int id, [FromBody] Location location)
+        {
+            location.LocationId = id;
+            _db.Entry(location).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        //DELETE api/locations/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var locationToDelete = _db.Locations.FirstOrDefault(entry => entry.LocationId == id);
+            _db.Locations.Remove(locationToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ ParkAPI/Controllers/ParksController.cs
@@ -0,0 +1,84 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ParkFinder.Models;
+
+namespace ParkFinder.Controllers{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParksController : ControllerBase
+    {
+        private ParkFinderContext _db;
+        public ParksController(ParkFinderContext db)
+        {
+            _db = db;
+        }
+
+        //GET api/parks
+        [HttpGet]
+        public ActionResult<IEnumerable<Park>> Get(string parkName, string fees, string alerts, bool hasHikingTrails, bool isCurrentlyOpen)
+        {
+            var query = _db.Parks.AsQueryable();
+
+            if (parkName != null)
+            {
+                query = query.Where(entry => entry.ParkName == parkName);
+            }
+
+            if (fees != null)
+            {
+                query = query.Where(entry => entry.Fees == fees);
+            }
+
+            if (alerts != null)
+            {
+                query = query.Where(entry => entry.Alerts == alerts);
+            }
+
+            if (hasHikingTrails == true)
+            {
+                query = query.Where(entry => entry.HasHikingTrails == hasHikingTrails);
+            }
+
+            if (isCurrentlyOpen == true)
+            {
+                query = query.Where(entry => entry.IsCurrentlyOpen == isCurrentlyOpen);
+            }
+
+            return query.ToList();
+        }
+
+        //POST api/parks
+        [HttpPost]
+        public void Post([FromBody] Park park)
+        {
+            _db.Parks.Add(park);
+            _db.SaveChanges();
+        }
+
+        //GET api/parks/5
+        [HttpGet("{id}")]
+        public ActionResult<Park> Get(int id)
+        {
+            return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+        }
+
+        //PUT api/parks/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Park park)
+        {
+            park.ParkId = id;
+            _db.Entry(park).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+            _db.Parks.Remove(parkToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
