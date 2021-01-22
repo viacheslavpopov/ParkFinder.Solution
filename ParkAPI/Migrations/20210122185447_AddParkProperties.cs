04f1e8474eef1082574656bef6ed47f662a3f0c0
ilromali <iliashenkoroman@gmail.com>
2021-01-22 10:55:13 -0800

    Add to park model & update migrations

---
+++ ParkAPI/Migrations/20210122185447_AddParkProperties.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkFinder.Solution.Migrations
+{
+    public partial class AddParkProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Alerts",
+                table: "Parks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BasicInfo",
+                table: "Parks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Directions",
+                table: "Parks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fees",
+                table: "Parks",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasHikingTrails",
+                table: "Parks",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasVisitorCenter",
+                table: "Parks",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "InternetAccess",
+                table: "Parks",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCurrentlyOpen",
+                table: "Parks",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParkAddress",
+                table: "Parks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RequiredPasses",
+                table: "Parks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TypeOfPark",
+                table: "Parks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Alerts",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "BasicInfo",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "Directions",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "Fees",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "HasHikingTrails",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "HasVisitorCenter",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "InternetAccess",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "IsCurrentlyOpen",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "ParkAddress",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "RequiredPasses",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "TypeOfPark",
+                table: "Parks");
+        }
+    }
+}
--- ParkAPI/Models/Park.cs
+++ ParkAPI/Models/Park.cs
@@ -4,6 +4,17 @@ namespace ParkFinder.Models
     {
         public int ParkId { get; set; }
         public int ParkName { get; set; }
+        public string ParkAddress { get; set; }
+        public string Directions { get; set; }
+        public int Fees { get; set; }
+        public string RequiredPasses { get; set; }
+        public string BasicInfo { get; set; }
+        public string Alerts { get; set; }
+        public string TypeOfPark { get; set; } //(Nat'l Historic Trail, National Park, Nat'l Historic Site, etc.)
+        public bool HasHikingTrails { get; set; } = true;
+        public bool HasVisitorCenter { get; set; } = false;
+        public bool IsCurrentlyOpen { get; set; } = true; //helpful for covid closures, stretch goal built in park hours, weather advisories, road closures, etc.
+        public bool InternetAccess { get; set; } = false; //stretch to string with internet & cell access info
 
     }
 }
