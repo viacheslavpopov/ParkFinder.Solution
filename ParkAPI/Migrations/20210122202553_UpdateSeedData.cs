cac9a3d9e5a1b8d59975d864e4d31e2fa2d2dfb0
ilromali <iliashenkoroman@gmail.com>
2021-01-22 12:26:24 -0800

    Update seed data details

---
+++ ParkAPI/Migrations/20210122202553_UpdateSeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkFinder.Solution.Migrations
+{
+    public partial class UpdateSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6,
+                column: "RequiredPasses",
+                value: "None required.");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6,
+                column: "RequiredPasses",
+                value: "Yes, day passes and annual passes available.");
+        }
+    }
+}
