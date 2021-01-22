1e45a6bceaf948074e01436ffcbbc3d12dbfbf33
ilromali <iliashenkoroman@gmail.com>
2021-01-22 12:19:21 -0800

    Add seed data

---
+++ ParkAPI/Migrations/20210122201856_AddSeedData.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkFinder.Solution.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "LocationId", "City", "Country", "Region", "State" },
+                values: new object[,]
+                {
+                    { 1, "CraterLake", "USA", "Southern Oregon", "OR" },
+                    { 2, "Kimberly", "USA", "Eastern Oregon", "OR" },
+                    { 3, "Cave Junction", "USA", "Southern Oregon", "OR" },
+                    { 4, "Marblemount", "USA", "Northern WA", "WA" },
+                    { 5, "Port Angeles", "USA", "Western WA", "WA" },
+                    { 6, "Friday Harbor", "USA", "NW WA", "WA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Alerts", "BasicInfo", "Directions", "Fees", "HasHikingTrails", "HasVisitorCenter", "InternetAccess", "IsCurrentlyOpen", "LocationId", "ParkAddress", "ParkName", "RequiredPasses", "TypeOfPark" },
+                values: new object[,]
+                {
+                    { 1, "Lots of snow! Chains required.", "Here's some history, geology, local knowledge", "TBD", "TBD", true, true, false, true, 1, "TBD", "Crater Lake", "Yes, day passes and annual passes available.", "National Park" },
+                    { 2, "Icy conditions. Drive with caution.", "Here's some history, geology, local knowledge", "TBD", "TBD", true, true, false, true, 2, "TBD", "John Day Fossil Beds", "Yes, day passes and annual passes available.", "National Monument" },
+                    { 3, "Landslide on road to OR caves visitor center.", "Here's some history, geology, local knowledge", "TBD", "TBD", true, true, false, true, 3, "TBD", "Oregon Caves", "Yes, day passes and annual passes available.", "National Monument & Preserve" },
+                    { 4, "CLosed for the season.", "Here's some history, geology, local knowledge", "TBD", "TBD", true, true, false, true, 4, "TBD", "North Cascades", "Yes, day passes and annual passes available.", "National Monument & Preserve" },
+                    { 5, "High tides. See X tide chart for more info.", "Here's some history, geology, local knowledge", "TBD", "TBD", true, false, false, true, 5, "TBD", "Olympic", "Yes, day passes and annual passes available.", "National Park" },
+                    { 6, "Ferry service delay on all afternoon boats 1.22.21. See WA State Ferries site for more info.", "Here's some history, geology, local knowledge", "TBD", "TBD", true, true, false, true, 6, "TBD", "San Juan Island", "Yes, day passes and annual passes available.", "National Monument & Preserve" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 6);
+        }
+    }
+}
--- ParkAPI/Models/ParkFinderContext.cs
+++ ParkAPI/Models/ParkFinderContext.cs
@@ -11,5 +11,29 @@ namespace ParkFinder.Models
 
             public DbSet<Park> Parks { get; set;}
             public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Location>()
+
+                .HasData(
+                    new Location { LocationId = 1, City = "CraterLake", State = "OR", Country = "USA", Region = "Southern Oregon" },
+                    new Location { LocationId = 2, City = "Kimberly", State = "OR", Country = "USA", Region = "Eastern Oregon"},
+                    new Location { LocationId = 3, City = "Cave Junction", State = "OR", Country = "USA", Region = "Southern Oregon" },
+                    new Location { LocationId = 4, City = "Marblemount", State = "WA", Country = "USA", Region = "Northern WA" },
+                    new Location { LocationId = 5, City = "Port Angeles", State = "WA", Country = "USA", Region = "Western WA" },
+                    new Location { LocationId = 6, City = "Friday Harbor", State = "WA", Country = "USA", Region = "NW WA" }
+                );
+            builder.Entity<Park>()
+
+                .HasData(
+                    new Park { ParkId = 1, ParkName = "Crater Lake", ParkAddress = "TBD", Directions = "TBD", Fees = "TBD", RequiredPasses = "Yes, day passes and annual passes available.", BasicInfo = "Here's some history, geology, local knowledge", Alerts = "Lots of snow! Chains required.", TypeOfPark = "National Park", HasHikingTrails = true, HasVisitorCenter = true, IsCurrentlyOpen = true, InternetAccess = false, LocationId = 1 },
+                    new Park { ParkId = 2, ParkName = "John Day Fossil Beds", ParkAddress = "TBD", Directions = "TBD", Fees = "TBD", RequiredPasses = "Yes, day passes and annual passes available.", BasicInfo = "Here's some history, geology, local knowledge", Alerts = "Icy conditions. Drive with caution.", TypeOfPark = "National Monument", HasHikingTrails = true, HasVisitorCenter = true, IsCurrentlyOpen = true, InternetAccess = false, LocationId = 2 },
+                    new Park { ParkId = 3, ParkName = "Oregon Caves", ParkAddress = "TBD", Directions = "TBD", Fees = "TBD", RequiredPasses = "Yes, day passes and annual passes available.", BasicInfo = "Here's some history, geology, local knowledge", Alerts = "Landslide on road to OR caves visitor center.", TypeOfPark = "National Monument & Preserve", HasHikingTrails = true, HasVisitorCenter = true, IsCurrentlyOpen = true, InternetAccess = false, LocationId = 3 },
+                    new Park { ParkId = 4, ParkName = "North Cascades", ParkAddress = "TBD", Directions = "TBD", Fees = "TBD", RequiredPasses = "Yes, day passes and annual passes available.", BasicInfo = "Here's some history, geology, local knowledge", Alerts = "CLosed for the season.", TypeOfPark = "National Monument & Preserve", HasHikingTrails = true, HasVisitorCenter = true, IsCurrentlyOpen = true, InternetAccess = false, LocationId = 4 },
+                    new Park { ParkId = 5, ParkName = "Olympic", ParkAddress = "TBD", Directions = "TBD", Fees = "TBD", RequiredPasses = "Yes, day passes and annual passes available.", BasicInfo = "Here's some history, geology, local knowledge", Alerts = "High tides. See X tide chart for more info.", TypeOfPark = "National Park", HasHikingTrails = true, HasVisitorCenter = false, IsCurrentlyOpen = true, InternetAccess = false, LocationId = 5 },
+                    new Park { ParkId = 6, ParkName = "San Juan Island", ParkAddress = "TBD", Directions = "TBD", Fees = "TBD", RequiredPasses = "Yes, day passes and annual passes available.", BasicInfo = "Here's some history, geology, local knowledge", Alerts = "Ferry service delay on all afternoon boats 1.22.21. See WA State Ferries site for more info.", TypeOfPark = "National Monument & Preserve", HasHikingTrails = true, HasVisitorCenter = true, IsCurrentlyOpen = true, InternetAccess = false, LocationId = 6 }
+                );
+        }
     }
 }
