34ed8f9517307bc67c014b2ef26a0c00259f8d83
ilromali <iliashenkoroman@gmail.com>
2021-01-24 22:21:50 -0800

    Comment out non-fxnal jwt code

--- ParkAPI/Controllers/SecurityController.cs
+++ ParkAPI/Controllers/SecurityController.cs
@@ -1,73 +1,73 @@
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using ParkAPI.Models;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Mvc;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Text;
+// using Microsoft.EntityFrameworkCore;
+// using System.Collections.Generic;
+// using System.Linq;
+// using ParkAPI.Models;
+// using System.Security.Claims;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.AspNetCore.Mvc;
+// using System.IdentityModel.Tokens.Jwt;
+// using Microsoft.IdentityModel.Tokens;
+// using System;
+// using System.Text;
 
-namespace ParkAPI.Controllers
-{
+// namespace ParkAPI.Controllers
+// {
 
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SecurityController : ControllerBase
-    {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class SecurityController : ControllerBase
+//     {
 
-        private string GenerateJWT()
-        {
-            var issuer = _config["Jwt:Issuer"];
-            var audience = _config["Jwt:Audience"];
-            var expiry = DateTime.Now.AddMinutes(120);
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+//         private string GenerateJWT()
+//         {
+//             var issuer = _config["Jwt:Issuer"];
+//             var audience = _config["Jwt:Audience"];
+//             var expiry = DateTime.Now.AddMinutes(120);
+//             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+//             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(issuer: issuer,
-                audience: audience,
-                expires: DateTime.Now.AddMinutes(120),
-                signingCredentials: credentials);
+//             var token = new JwtSecurityToken(issuer: issuer,
+//                 audience: audience,
+//                 expires: DateTime.Now.AddMinutes(120),
+//                 signingCredentials: credentials);
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var stringToken = tokenHandler.WriteToken(token);
-        }
+//             var tokenHandler = new JwtSecurityTokenHandler();
+//             var stringToken = tokenHandler.WriteToken(token);
+//         }
 
-        private bool ValidateUser(User loginDetails)
-        {
-            if (loginDetails.UserName == "User1" && loginDetails.Password=="pass$word")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+//         private bool ValidateUser(User loginDetails)
+//         {
+//             if (loginDetails.UserName == "User1" && loginDetails.Password=="pass$word")
+//             {
+//                 return true;
+//             }
+//             else
+//             {
+//                 return false;
+//             }
+//         }
 
-        public class User
-        {
-            public string UserName { get; set; }
-            public string Password { get; set; }
-        }
+//         public class User
+//         {
+//             public string UserName { get; set; }
+//             public string Password { get; set; }
+//         }
 
-        [HttpPost]
-        public IActionResult Login([FromBody]User loginDetails)
-        {
-            bool result = ValidateUser(loginDetails);
-            if (result)
-            {
-            var tokenString = GenerateJWT();
-            return Ok(new { token = tokenString });
-            }
-            else
-            {
-                return Unauthorized();
-            }
-        }
-    }
-}
+//         [HttpPost]
+//         public IActionResult Login([FromBody]User loginDetails)
+//         {
+//             bool result = ValidateUser(loginDetails);
+//             if (result)
+//             {
+//             var tokenString = GenerateJWT();
+//             return Ok(new { token = tokenString });
+//             }
+//             else
+//             {
+//                 return Unauthorized();
+//             }
+//         }
+//     }
+// }
